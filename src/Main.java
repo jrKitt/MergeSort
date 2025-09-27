72714d890c46d9068f5ae2cdb2062598e02bc08f
jrKitt <kittichdev@gmail.com>
2025-09-27 15:39:45 +0700

    MergeSort Code

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("input data size: ");
+        System.out.print("Input data size: ");
         int n = sc.nextInt();
 
         int[] arr = new int[n];
 
-        System.out.println("Element " + n + ":");
+        System.out.print("Enter " + n + " Elements:");
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
